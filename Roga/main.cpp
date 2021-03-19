fb063bcfa9d8c67955cadfea4759b2538a11c21a
armear3000 <armear3000@yandex.ru>
2021-03-19 15:46:39 +0300

    0.2

---
+++ Roga/main.cpp
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <cstdlib>
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_primitives.h>
+#include <allegro5/allegro5.h>
+#include <allegro5/allegro_image.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_ttf.h>
+#include <allegro5/allegro_native_dialog.h>
+#include <math.h>
+#include <stdio.h> 
+#include <conio.h>
+#include <stdlib.h>
+
+int main() {
+
+
+	return 0;
+}
